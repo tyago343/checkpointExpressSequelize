941040fd33f2ea00c8a8bf002018856b4a8c65d4
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-10-03 17:10:12 -0300

    first commit

---
+++ models/article.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var db = require('./database');
+var Sequelize = require('sequelize');
+
+// Make sure you have `postgres` running!
+
+var User = require('./user');
+
+//---------VVVV---------  your code below  ---------VVV----------
+
+var Article = db.define('article', {
+
+
+});
+
+//---------^^^---------  your code above  ---------^^^----------
+
+module.exports = Article;
